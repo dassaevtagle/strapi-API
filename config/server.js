d43001829ed8c1e8037b5b983f463f360b9b1573
dassaevtagle <dassaev.tagle@gmail.com>
2022-03-14 21:42:40 -0600

    Add config vars

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [process.env.KEY_1, process.env.KEY_2]),
   },
 });
